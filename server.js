d5cd94085e06fb66d179282d4b23e3f16e231daf
Omarfaruk05 <mdomarfrauk149518.com>
2022-12-10 18:16:16 +0600

    vercel

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv').config();
-const colors = require('colors');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+const colors = require("colors");
 
-const app = require('./app');
+const app = require("./app");
 
-mongoose.connect(process.env.DATABASE, {
+mongoose
+  .connect(process.env.DATABASESERVER, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
-})
-.then(()=> {
+  })
+  .then(() => {
     console.log(`Database connection is successfull.`.red.bold);
-});
+  });
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-    console.log(`App is running on port ${port}`.green.bold);
+  console.log(`App is running on port ${port}`.green.bold);
 });
